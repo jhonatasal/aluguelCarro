7c24c01b12718ad1ae723cea0dbadff6d9f395fc
Jhonatas Alves <jhonatasa_s@hotmail.com>
2019-09-19 15:14:07 -0300

    Projeto com Interface

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -7,9 +7,9 @@ public class RentalService {
 	private Double pricePerDay;
 	private Double pricePerHour;
 
-	private BrazilTaxService taxService;
+	private TaxService taxService;
 
-	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+	public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
 		this.pricePerDay = pricePerDay;
 		this.pricePerHour = pricePerHour;
 		this.taxService = taxService;
@@ -18,13 +18,16 @@ public class RentalService {
 	public void processInvoice(CarRental carRental) {
 		long t1 = carRental.getStart().getTime();
 		long t2 = carRental.getFinish().getTime();
-		double hours = ((double) t2 - t1) / 1000 / 60 / 60;
+		double hours = (double)(t2 - t1) / 1000 / 60 / 60;
+		
 		double basicPayment;
 		if (hours <= 12.0) {
-			basicPayment = Math.ceil(hours) * pricePerHour;
-		} else {
-			basicPayment = Math.ceil(hours) * pricePerDay;
+			basicPayment = pricePerHour * Math.ceil(hours);
 		}
+		else {
+			basicPayment = pricePerDay * Math.ceil(hours / 24);
+		}
+
 		double tax = taxService.tax(basicPayment);
 
 		carRental.setInvoice(new Invoice(basicPayment, tax));
